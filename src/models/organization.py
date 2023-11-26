98e378e32bf4c5851b506f64faa8d2e9630ef2dc
Ayush Kumar <ayushkmr484@gmail.com>
2023-11-26 21:16:25 +0530

    Update organization name with filtered url

--- src/models/organization.py
+++ src/models/organization.py
@@ -31,7 +31,23 @@ class Organization:
                 a dictionary containing the organization's data
         """
         self._url = data.get('url')
-        self._name = self._url
+        self._name = self.clean_url(self._url)
+
+    @staticmethod
+    def clean_url(url: str) -> str:
+        """
+        Cleans up a URL by removing leading '/company/' and trailing '/'.
+
+        Args:
+            url (str): A URL string to be cleaned.
+
+        Returns:
+            str: A cleaned URL string.
+        """
+        if not url: 
+            return url
+        url = url.replace('/company/', '')
+        return url.rstrip('/')
 
     @property
     def url(self):
@@ -40,18 +56,19 @@ class Organization:
 
     @url.setter
     def url(self, value):
-        """ Updates the url of the organization """
-        self._name = self._url = value
+        """ Updates the url of the organization and also updates the name accordingly """
+        self._url = value
+        self._name = self.clean_url(value)
 
     @property
     def name(self):
-        """ Returns the name of the organization """
+        """ Returns the name (cleaned url) of the organization """
         return self._name
 
     @name.setter
     def name(self, value):
-        """ Updates the name of the organization """
-        self._name = self._url = value
+        """ Raises an exception as name is the cleaned version of URL and shouldn't be manually set """
+        raise Exception("Can't set name directly. It's derived from URL.")
 
     def to_dict(self):
         """ Returns the dictionary representation of the organization """
